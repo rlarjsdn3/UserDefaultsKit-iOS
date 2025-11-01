5a02fae65dc1f48d01b5e49738c8049e1545b9d0
rlarjsdn3 <rlarjsdn3@naver.com>
2025-11-01 12:00:23 +0900

    feat: 프로젝트 생성 및 초기 설정

---
+++ Sources/UserDefaultsWrapper.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsWrapper.swift
+//  Pods-UserDefaultsWrapper_Tests
+//
+//  Created by 김건우 on 11/1/25.
+//
+
+import Foundation
+
+final class UserDefaultsWrapper {
+    
+}
---
+++ Tests/UserDefaultsWrapper_Tests.swift
@@ -0,0 +1,24 @@
+//
+//  UserDefaultsWrapper_Tests.swift
+//  UserDefaultsWrapper_Tests
+//
+//  Created by 김건우 on 11/1/25.
+//  Copyright © 2025 CocoaPods. All rights reserved.
+//
+
+import Testing
+
+final class UserDefaultsWrapper_Tests {
+    
+    init() {
+        
+    }
+    
+    deinit {
+        
+    }
+    
+    @Test func example() {
+        #expect(true)
+    }
+}
