a45d40311763284165b9002b4cb39ddfb6a84be3
김소월 <rlarjsdn3@naver.com>
2025-11-03 10:07:18 +0900

    feat: UserDefaultsWrapper 및 Key 관련 코드 작성 (#2)

--- Sources/UserDefaultsWrapper.swift
+++ Sources/UserDefaultsWrapper.swift
@@ -7,6 +7,94 @@
 
 import Foundation
 
-final class UserDefaultsWrapper {
+///
+@dynamicMemberLookup
+final public class UserDefaultsWrapper: @unchecked Sendable {
     
+    // MARK: - Shared
+    
+    ///
+    public static let shared = UserDefaultsWrapper()
+    
+    
+    // MARK: - Intializer
+    
+    ///
+    private init() {
+        userDefaults = UserDefaults()
+    }
+    
+    ///
+    public init(suitName: String) {
+        userDefaults = UserDefaults(suiteName: suitName)
+    }
+    
+    
+    // MARK: - Properties
+    
+    ///
+    private let userDefaults: UserDefaults!
+    
+    
+    // MARK: - Get
+    
+    ///
+    public func get<Value>(forKey keyPath: KeyPath<UserDefaultsWrapperKeys, UserDefaultsWrapperKey<Value>>) -> Value where Value: Codable {
+        let key = UserDefaultsWrapperKeys()[keyPath: keyPath]
+        return get(forKey: key.name) ?? key.default
+    }
+    
+    ///
+    public func get<Value>(forKey key: String) -> Value? where Value: Codable {
+        userDefaults.object(forKey: key) as? Value
+    }
+    
+    
+    // MARK: - Set
+    
+    ///
+    public func set<Value>(
+        _ value: Value?,
+        forKey keyPath: KeyPath<UserDefaultsWrapperKeys, UserDefaultsWrapperKey<Value>>,
+    ) where Value: Codable {
+        let key = UserDefaultsWrapperKeys()[keyPath: keyPath]
+        set(value, forKey: key.name)
+    }
+    
+    ///
+    public func set<Value>(
+        _ value: Value?,
+        forKey key: String
+    ) where Value: Codable {
+        userDefaults.set(value, forKey: key)
+    }
+    
+    
+    // MARK: - Remove
+    
+    ///
+    public func remove<Value>(forKey keyPath: KeyPath<UserDefaultsWrapperKeys, UserDefaultsWrapperKey<Value>>) {
+        let key = UserDefaultsWrapperKeys()[keyPath: keyPath]
+        remove(forKey: key.name)
+    }
+    
+    ///
+    public func remove(forKey key: String) {
+        userDefaults.removeObject(forKey: key)
+    }
+    
+    ///
+    public func removeAll() {
+        let dict = userDefaults.dictionaryRepresentation()
+        dict.keys.forEach { key in remove(forKey: key) }
+    }
+}
+
+extension UserDefaultsWrapper {
+    
+    ///
+    public subscript<Value>(dynamicMember keyPath: KeyPath<UserDefaultsWrapperKeys, UserDefaultsWrapperKey<Value>>) -> Value? where Value: Codable {
+        get { get(forKey: keyPath) }
+        set { set(newValue, forKey: keyPath) }
+    }
 }
